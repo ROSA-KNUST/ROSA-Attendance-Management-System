332d7037d199c1b5c44fc6c8f9c10ebdede4b15b
Eric Mills <ericlankwei@gmail.com>
2025-11-15 18:21:47 +0000

    updated the user-pages

---
+++ FRONTEND/main.js
@@ -0,0 +1,2 @@
+//place all javascript codes here, dont create a seperate page & remember to give a comment at the top of your code snipet(not each line) indicating which page that code belongs to.
+
